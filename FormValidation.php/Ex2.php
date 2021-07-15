08c6eb702915d55e16d475fc07cf9a3c65294c0a
ChornPenghong <pengh202@gmail.com>
2021-07-15 17:52:48 +0700

    NEW

--- FormValidation.php/Ex2.php
+++ FormValidation.php/Ex2.php
@@ -2,12 +2,12 @@
 <html>
     <body>
 
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <form method="GET" action="<?php echo $_SERVER['PHP_SELF'];?>">
     Name: <input type="text" name="fname">
     <input type="submit">
     </form>
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
             // collect value of input field
             $name = htmlspecialchars($_REQUEST['fname']);
             if (empty($name)) {
@@ -16,6 +16,6 @@
                 echo $name;
             }
         }
-    ?>
+    ?> 
     </body>
 </html>
